1e49499a365a7fed2981baf22c10af0fb6802060
Peter Adelmann <peter.adelmann@trilobit.de>
2022-04-14 14:59:22 +0200

    some improvements

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -9,11 +9,13 @@ declare(strict_types=1);
  * @link       http://github.com/trilobit-gmbh/contao-jointforms-bundle
  */
 
+$GLOBALS['TL_LANG']['tl_member']['jf_legend'] = 'Jointforms';
+
+$GLOBALS['TL_LANG']['tl_member']['jf_last_modified'][0] = 'Date modified';
+$GLOBALS['TL_LANG']['tl_member']['jf_last_modified'][1] = 'Date and time Jointforms was last modified.';
 $GLOBALS['TL_LANG']['tl_member']['jf_data'][0] = 'Jointforms Data';
 $GLOBALS['TL_LANG']['tl_member']['jf_data'][1] = 'Data as JSON.';
 $GLOBALS['TL_LANG']['tl_member']['jf_complete'][0] = 'Completed';
 $GLOBALS['TL_LANG']['tl_member']['jf_complete'][1] = 'Complete submitted Jointforms.';
-$GLOBALS['TL_LANG']['tl_member']['jf_complete_datim'][0] = 'Completed on';
+$GLOBALS['TL_LANG']['tl_member']['jf_complete_datim'][0] = 'Date completed';
 $GLOBALS['TL_LANG']['tl_member']['jf_complete_datim'][1] = 'Date and time Jointforms was completed.';
-
-$GLOBALS['TL_LANG']['tl_member']['jf_legend'] = 'Jointforms';
