30f01f598f6a9127c8fd76fe78d1c24f11fe21ba
Peter Adelmann <peter.adelmann@trilobit.de>
2024-05-14 16:58:05 +0200

    fix php8 issues

--- src/DataContainer/Edit.php
+++ src/DataContainer/Edit.php
@@ -19,26 +19,32 @@ class Edit
 {
     public static function jsonView(DataContainer $dc): string
     {
-        return '<div class="widget clr">'
-            .'<h3>'.$GLOBALS['TL_LANG']['tl_member']['jf_data'][0].'</h3>'
-            .'<div style="height:auto;background:#f3f3f5;padding:6px;border:1px solid #aaa" class="json-tree">'
-            .'</div>'
-            .'<textarea name="jf_data" id="ctrl_jf_data" class="tl_textarea" rows="12" cols="80" data-action="focus->contao--scroll-offset#store" data-contao--scroll-offset-target="autoFocus">'.$dc->activeRecord->jf_data.'</textarea>'
+        $buffer = '<div class="widget clr">'
+            .'<h3>'.$GLOBALS['TL_LANG']['tl_member']['jf_data'][0].'</h3>';
+
+        if (!empty($dc->activeRecord->jf_data)) {
+            $buffer .= '<div style="height:auto;background:#f3f3f5;padding:6px;border:1px solid #aaa" class="json-tree">'
+                .'</div>'
+                .'<script src="/bundles/trilobitjointforms/jsonview/dist/jsonview.js"></script>'
+                .'<script>'
+                    .'jsonview.render('
+                    .'jsonview.create('
+                        .str_replace(
+                            '&#34;',
+                            '"',
+                            $dc->activeRecord->jf_data ?? ''
+                        )
+                    .'),'
+                    .'document.querySelector(\'.json-tree\')'
+                    . ')'
+                .'</script>'
+                .'<style>.json-container{background:transparent}.json-container .line{word-break:break-all}.json-container .json-key{word-break:normal}</style>';
+        }
+
+        $buffer .= '<textarea name="jf_data" id="ctrl_jf_data" class="tl_textarea" rows="12" cols="80" data-action="focus->contao--scroll-offset#store" data-contao--scroll-offset-target="autoFocus">'.$dc->activeRecord->jf_data.'</textarea>'
             .'<p class="tl_help tl_tip" title="">'.$GLOBALS['TL_LANG']['tl_member']['jf_data'][1].'</p>'
-            .'</div>'
-            .'<script src="/bundles/trilobitjointforms/jsonview/dist/jsonview.js"></script>'
-            .'<script>'
-            .'jsonview.render('
-            .'jsonview.create('
-            .str_replace(
-                '&#34;',
-                '"',
-                $dc->activeRecord->jf_data
-            )
-            .'),'
-            .'document.querySelector(\'.json-tree\')'
-            . ')'
-            .'</script>'
-            .'<style>.json-container{background:transparent}.json-container .line{word-break:break-all}.json-container .json-key{word-break:normal}</style>';
+            .'</div>';
+
+        return $buffer;
     }
 }
